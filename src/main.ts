4c130b801b8f256f9849ea5fb6e939f10c60f687
vamseeuxd <vamsee.uxd@gmail.com>
2023-05-28 09:08:46 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -15,16 +15,22 @@ import { bootstrapApplication } from '@angular/platform-browser';
   <ng-template #planingList let-options="options">
     <ul class="list-group shadow-sm">
       <ng-container *ngFor="let option of options; let ind=index">
-        <li class="list-group-item shadow-sm rounded-0">
+        <li class="list-group-item shadow-sm rounded-0" [style.opacity]="copiedItem?.id == option.id ? 1 : 1">
             <div class="d-flex justify-content-center align-items-center">
               <i (click)="option.open = !option.open" role="button" [class.fa-folder-o]="!option.open" [class.fa-folder-open-o]="option.open" class="fa m-0 p-0 me-2"></i> 
-              <div class="d-flex w-100 me-3">
-                <label class="col-6 text-danger">{{option.name}}</label>
+              <div class="col d-flex me-3 ms-2">
+                <label class="col-6 text-dark">{{option.name}}</label>
                 <label class="col-6 text-success text-end fw-bold">{{option.amount | currency : 'INR'}}</label>
               </div>
-              <button role="button" (click)="options.splice(ind, 1)" class="btn btn-outline-danger shadow-sm btn-sm ms-auto">
-              <i class="fa fa-trash"></i>
-              </button>
+              <div class="ms-auto" *ngIf="!copiedItem">
+                <button role="button" title="Delete" (click)="options.splice(ind, 1)" class="btn btn-outline-danger me-1 shadow-sm btn-sm"><i class="fa fa-trash"></i></button>
+                <button role="button" title="Copy" (click)="copiedItem = option; action = 'copy'" (click)="options.splice(ind, 1)" class="btn btn-outline-warning ms-1 shadow-sm btn-sm"><i class="fa fa-copy"></i></button>
+              </div>
+              <div class="ms-auto" *ngIf="copiedItem">
+                <button role="button" title="Cancel Copy" (click)="copiedItem = null" class="btn btn-outline-danger me-1 shadow-sm btn-sm"><i class="fa fa-times"></i></button>
+                <button role="button" title="Paste Below" (click)="pasteItem(options,ind+1)" class="btn btn-outline-primary me-1 shadow-sm btn-sm"><i class="fa fa-arrow-down"></i></button>
+                <button role="button" title="Paste Inside" (click)="pasteItem(option.children,option.children.length - 1)" class="btn btn-outline-primary ms-1 shadow-sm btn-sm"><i class="fa fa-indent"></i></button>
+              </div>
             </div>
         </li>
         <ng-container *ngIf="option.children && option.open">
@@ -33,28 +39,31 @@ import { bootstrapApplication } from '@angular/platform-browser';
           </li>
         </ng-container>
       </ng-container>   
-      <li class="list-group-item mt-2 border-top shadow-sm rounded-0">
+      <li class="list-group-item mt-2 border-top shadow-sm rounded-0" *ngIf="!copiedItem">
         <div class="input-group">
           <input type="text" class="form-control me-1" placeholder="Name" #nameRef (keyup.enter)="addItem( options, amountRef, nameRef )">
           <input type="number" class="form-control ms-1" placeholder="Amount" #amountRef (keyup.enter)="addItem( options, amountRef, nameRef )">
         </div>
       </li>   
     </ul>
-    <pre>{{data|json}}</pre>
-  </ng-template>
+    </ng-template>
   `,
 })
 export class App {
+  copiedItem: any | null = null;
+  action: 'cut' | 'copy' | null = null;
   data = [
     {
+      id: 1,
       open: false,
-      name: 'asdfasdfas',
+      name: 'Test 123',
       amount: '500',
       children: [],
     },
     {
+      id: 2,
       open: false,
-      name: 'asdfasdfasdf',
+      name: 'Test 456',
       amount: '1000',
       children: [],
     },
@@ -66,6 +75,7 @@ export class App {
   ) {
     if (amountRef.value && nameRef.value) {
       options.push({
+        id: new Date().getTime(),
         open: false,
         name: nameRef.value,
         amount: Number(amountRef.value),
@@ -73,10 +83,15 @@ export class App {
       });
       amountRef.value = '';
       nameRef.value = '';
-    } else {
-      alert('Provide Valid Name and Amount');
+      nameRef.focus();
+    } else if (!amountRef.value) {
+      amountRef.focus();
+    } else if (!nameRef.value) {
+      nameRef.focus();
     }
-    nameRef.focus();
+  }
+  pasteItem(options: any[], index: number) {
+    options.splice(index, 0, { ...this.copiedItem, id: new Date().getTime() });
   }
 }
 
